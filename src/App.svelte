6f71f4ba8d13fe653efd32eccfe00971a562bbb9
multics <multics.cn@gmail.com>
2023-05-01 11:39:24 +0800

    init

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+import Counter from './lib/Counter.svelte'
+</script>
+
+<div class="flex h-screen flex-col justify-center">
+  <h1 class="text-center text-3xl">hello there!</h1>
+  <p class="text-center text-4xl font-bold text-blue-400">start from here: src/App.svelte</p>
+  <Counter />
+</div>
+
+<style lang="postcss">
+:global(html) {
+  background-color: theme(colors.green.100);
+}
+</style>
